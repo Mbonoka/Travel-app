6dfe060e76764ca21eb92d37ce036709f33e926c
Mbonoka <esther.mbonoka@strathmore.edu>
2025-04-09 13:28:28 +0300

    Hotel

---
+++ lib/homepage_destinations.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'details_page_header.dart';
+
+class DestinationList extends StatelessWidget {
+  const DestinationList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const DetailsPage()),
+            );
+          },
+          child: Card(
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Image
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12),
+                  ),
+                  child: Image.asset(
+                    '/assets/images/download.jpeg',
+                    height: 200,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Jambo Hotel',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      const Text(
+                        'Kileleshwa, Kenya • 5 km',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      const SizedBox(height: 4),
+                      Row(
+                        children: const [
+                          Icon(Icons.star, color: Colors.yellow, size: 16),
+                          Icon(Icons.star, color: Colors.yellow, size: 16),
+                          Icon(Icons.star, color: Colors.yellow, size: 16),
+                          Icon(Icons.star, color: Colors.yellow, size: 16),
+                          Icon(
+                            Icons.star_border,
+                            color: Colors.yellow,
+                            size: 16,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 4),
+                      const Text(
+                        'KSH 25000 per night',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
